31315ec9c3fa1c4774d72077e57243b89032df46
Kael Soares Augusto <kaelsa@hotmail.com.br>
2023-11-25 14:09:30 -0300

    Included API and CLI; Moved recommender into it's own folder.

---
+++ CLI/cli.py
@@ -0,0 +1,18 @@
+import sys, os, json
+
+site = "http://127.0.0.1:32194"
+
+def usage():
+    print("The correct usage of this app is python cli.py \"song a, song b, song c\"")
+    exit()
+
+argv = sys.argv
+
+if(len(argv) != 2):
+    usage()
+
+songs = json.dumps({'songs': argv[1].split(", ")})
+
+cmd = "wget --header='Content-Type: application/json' --post-data '" + str(songs) + "' " + site + "/api/recommend/ -q -O -"
+
+os.system(cmd)
